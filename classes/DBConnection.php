97f7b91390defad751f555f0673ea60d3fc3ce07
Emma Poussette <77481598+Emmapoussette@users.noreply.github.com>
2022-05-09 16:01:03 +0200

    make it better

--- classes/DBConnection.php
+++ classes/DBConnection.php
@@ -1,31 +1,13 @@
 <?php
-if(!defined('DB_SERVER')){
-    require_once("../initialize.php");
-}
-class DBConnection{
-
-    private $host = DB_SERVER;
-    private $username = DB_USERNAME;
-    private $password = DB_PASSWORD;
-    private $database = DB_NAME;
-    
-    public $conn;
-    
-    public function __construct(){
-
-        if (!isset($this->conn)) {
-            
-            $this->conn = new mysqli($this->host, $this->username, $this->password, $this->database);
-            
-            if (!$this->conn) {
-                echo 'Cannot connect to database server';
-                exit;
-            }            
-        }    
-        
-    }
-    public function __destruct(){
-        $this->conn->close();
-    }
-}
+ require_once("../initialize.php");
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 ?>
