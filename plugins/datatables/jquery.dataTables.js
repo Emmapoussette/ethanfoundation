56ef1dfbb6c6f3ae98c4a320ad7808977f006af7
Emma Poussette <77481598+Emmapoussette@users.noreply.github.com>
2022-05-10 10:36:41 +0200

    make it better

--- plugins/datatables/jquery.dataTables.js
+++ plugins/datatables/jquery.dataTables.js
@@ -6499,7 +6499,7 @@
 	
 		if ( tn ) {
 			msg += '. For more information about this error, please see '+
-			'http://datatables.net/tn/'+tn;
+			'https://datatables.net/tn/'+tn;
 		}
 	
 		if ( ! level  ) {
