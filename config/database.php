5aaa7aecb741a60989aa26d86eb830540718ace3
Emma Poussette <77481598+Emmapoussette@users.noreply.github.com>
2022-05-11 14:22:47 +0200

    make it better

--- config/database.php
+++ config/database.php
@@ -1,7 +1,7 @@
 <?php
 
-const DB_HOST = 'localhost';
-const DB_NAME = 'ethan';
-const DB_USER = 'root';
-const DB_PASSWORD = '';
+const DB_HOST = 'us-cdbr-east-05.cleardb.net';
+const DB_NAME = 'heroku_4efddbbb968d167';
+const DB_USER = 'bec3b039073a93';
+const DB_PASSWORD = 'a1c039db';
 
