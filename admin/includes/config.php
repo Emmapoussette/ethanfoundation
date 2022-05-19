a568e59cc37ebccbd8b0afda33ba09689b00f50a
Emma Poussette <77481598+Emmapoussette@users.noreply.github.com>
2022-05-19 17:46:15 +0200

    make it better

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -1,5 +1,5 @@
 <?php
-/*define('DB_SERVER','us-cdbr-east-05.cleardb.net');
+define('DB_SERVER','us-cdbr-east-05.cleardb.net');
 define('DB_USER','bcd8c189837280');
 define('DB_PASS' ,'1897ad3f');
 define('DB_NAME','heroku_4bf53487c839d3e');
@@ -9,16 +9,5 @@ if (mysqli_connect_errno())
 {
  echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
-?>*/
-
-define('DB_SERVER','localhost');
-define('DB_USER','root');
-define('DB_PASS' ,'');
-define('DB_NAME','ethan');
-$conn = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
-// Check connection
-if (mysqli_connect_errno())
-{
- echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
 ?>
+
