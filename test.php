e1fb5d4a88a943ec205fba169877d7e6ebd60cca
Emma Poussette <77481598+Emmapoussette@users.noreply.github.com>
2022-05-19 16:18:23 +0200

    make it better

--- test.php
+++ test.php
@@ -1,255 +1,122 @@
-<!doctype html>
+<?php 
+session_start();
+include('initialize.php');
+
+    ?>
+
+<!DOCTYPE html>
 <html lang="en">
-<head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<title>Arunil Digital  || NGO </title>
-<link rel="shortcut icon" href="assets/images/fav.png" type="image/x-icon">
-<link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;500&display=swap" rel="stylesheet">
-<link rel="shortcut icon" href="assets/images/fav.jpg">
-<link rel="stylesheet" href="assets/css/bootstrap.min.css">
-<link rel="stylesheet" href="assets/css/all.min.css">
-<link rel="stylesheet" href="assets/css/animate.css">
-<link rel="stylesheet" href="assets/plugins/slider/css/owl.carousel.min.css">
-<link rel="stylesheet" href="assets/plugins/slider/css/owl.theme.default.css">
-<link rel="stylesheet" type="text/css" href="assets/css/style.css" />
+
+ 
+    <link rel="icon" href="icon_path" type="image/icon type">
+    <title>Ethan-Foundation| Updates</title>
+    <link rel="shortcut icon" href="images/favicon.ico" />
+        <link rel="icon" href="http://www.domain.com/favicon.ico" type="image/x-icon" />
+<link rel="shortcut icon" href="http://www.domain.com/favicon.ico" type="image/x-icon" />    
+        <!-- App css -->
+      <!--  <link href="../admin/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="../admin/assets/css/core.css" rel="stylesheet" type="text/css" />
+        <link href="../admin/assets/css/components.css" rel="stylesheet" type="text/css" />
+        <link href="../admin/assets/css/icons.css" rel="stylesheet" type="text/css" />
+        <link href="../admin/assets/css/pages.css" rel="stylesheet" type="text/css" />
+        <link href="../admin/assets/css/menu.css" rel="stylesheet" type="text/css" />
+        <link href="../admin/assets/css/responsive.css" rel="stylesheet" type="text/css" />
+		<link rel="stylesheet" href="plugins/switchery/switchery.min.css">
+        <script src="../admin/assets/js/modernizr.min.js"></script>-->
+
+
+  </head>
+
+  <body>
+
+    <!-- Navigation -->
+   <?php include('header.php');?>
+
+    <!-- Page Content -->
+    <div class="container">
+
+
+     
+      <div class="row" style="margin-top: 4%">
+
+        <!-- Blog Entries Column -->
+        <div class="col-md-8">
+
+          <!-- Blog Post -->
+<?php 
+     if (isset($_GET['pageno'])) {
+            $pageno = $_GET['pageno'];
+        } else {
+            $pageno = 1;
+        }
+        $no_of_records_per_page = 8;
+        $offset = ($pageno-1) * $no_of_records_per_page;
+
+
+        $total_pages_sql = "SELECT COUNT(*) FROM events";
+        $result = mysqli_query($conn,$total_pages_sql);
+        $total_rows = mysqli_fetch_array($result)[0];
+        $total_pages = ceil($total_rows / $no_of_records_per_page);
+
+
+$query=mysqli_query($con,"select events.id as eid,events.EventTitle as eventtitle,events.EventImage,category.CategoryName as category,category.id as
+ cid,events.EventDetails as eventdetails,events.PostingDate as postingdate, left join category on category.id=posts.CategoryId where posts.Is_Active=1 order by events.id desc  LIMIT $offset, $no_of_records_per_page");
+while ($row=mysqli_fetch_array($query)) {
+?>
+
+          <div class="card mb-4">
+ <img class="card-img-top" src="admin/assets/<?php echo htmlentities($row['EventImage']);?>" alt="<?php echo htmlentities($row['eventtitle']);?>">
+            <div class="card-body">
+              <h2 class="card-title"><?php echo htmlentities($row['eventtitle']);?></h2>
+                 <p><!--category-->
+ <a class="badge bg-secondary text-decoration-none link-light" href="category.php?catid=<?php echo htmlentities($row['cid'])?>" style="color:#fff"><?php echo htmlentities($row['category']);?></a>
+<!--Subcategory--->
+  </p>
+       
+              <a href="events-details.php?nid=<?php echo htmlentities($row['eid'])?>" class="btn btn-primary">Read More &rarr;</a>
+            </div>
+            <div class="card-footer text-muted">
+              Posted on <?php echo htmlentities($row['postingdate']);?>
+           
+            </div>
+          </div>
+<?php } ?>
+       
+
+      
+
+          <!-- Pagination -->
+
+
+    <ul class="pagination justify-content-center mb-4">
+        <li class="page-item"><a href="?pageno=1"  class="page-link">1</a></li>
+        <li class="page-item"><a href="?pageno=2"  class="page-link">2</a></li>
+        <li class="page-item"><a href="?pageno=3"  class="page-link">3</a></li>
+        <li class="<?php if($pageno <= 1){ echo 'disabled'; } ?> page-item">
+            <a href="<?php if($pageno <= 1){ echo '#'; } else { echo "?pageno=".($pageno - 1); } ?>" class="page-link">Prev</a>
+        </li>
+        <li class="<?php if($pageno >= $total_pages){ echo 'disabled'; } ?> page-item">
+            <a href="<?php if($pageno >= $total_pages){ echo '#'; } else { echo "?pageno=".($pageno + 1); } ?> " class="page-link">Next</a>
+        </li>
+        <li class="page-item"><a href="?pageno=<?php echo $total_pages; ?>" class="page-link">Last</a></li>
+    </ul>
+
+    
+      <!-- /.row -->
+
+    </div>
+    <!-- /.container -->
+
+    <!-- Footer -->
+      <?php include('footer.php');?>
+
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+ 
 </head>
-<body style="">
-<style>
-               
-               body{
-                   
-                   position: relative; min-height: 100%; margin-top: -40px;
-               }
-                   header .header-top {
-    background-color: #fff;
-    border-bottom: 1px solid #d3d7cea6;
-    padding: 7px;
-    padding-top: 40px;
-}
-        
-                 </style>
-<header class="continer-fluid ">
-<div class="header-top">
-<div class="container">
-<div class="row col-det">
-<div class="col-lg-5 d-none d-lg-block">
-<ul class="ulleft">
-<li>
-<i class="far fa-envelope"></i>
-<a href="/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="6e0009015f2e09030f0702400d0103">[email&#160;protected]</a> <span>|</span></li>
-<li>
-<i class="fas fa-phone-volume"></i>
-9079646229</li>
-</ul>
-</div>
-<style>
-                    
-                    
- iframe.skiptranslate {
-            display: none !important;
-        }
+  </body>
 
-        .skiptranslate.goog-te-gadget select {
-            position: relative !important;
-      
-            position: relative !important;
-            font-weight: bold !important;
-            padding: 2px !important;
-            border-radius: 5px !important;
-        }
-
-        .skiptranslate.goog-te-gadget span {
-            display: none !important;
-        }
-        
-        
-        .goog-te-gadget {
-    color: white;
-    font-family: arial;
-    font-size: 11px;
-    color: white;
-    white-space: nowrap;
-}
-    </style>
-<script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script type="text/javascript" src="https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
-<script>
-        function googleTranslateElementInit() {
-            //new google.translate.TranslateElement({ pageLanguage: 'en', includedLanguages: "hi,en" }, 'google_translate_element');
-            new google.translate.TranslateElement({}, 'google_translate_element');
-        }
-    </script>
-
-</header>
-
-
-<div class="slider">
-<div class="owl-carousel ">
-<div class="slider-img">
-<div class="item active ">
-<div class="slider-img"><img src="slide/32.jpg" style=" "></div></div> </div>
-<div class="item"> <div class="slider-img"> <img src="slide/31.jpg" style=" "></div></div>
-</div>
-</div>
-
-
-
-<section class="our-blog">
-<div class="container">
-<div class="row session-title">
-<h2> Our Blog </h2>
-<p>Take a look at what people say about US </p>
-</div>
-<div class="blog-row row">
-<div class='col-md-4 col-sm-6'>
-<div class='single-blog'>
-<figure>
-<img src='slide/image_06.jpg'>
-</figure>
-<div class='blog-detail'>
-<small>By Admin | Sat 15th May 2021</small>
-<h4>Role Of Women Empowerment And Gender Equ</h4>
-<p> <p>Women empowerment is a serious issue and needs a holistic approach so that rather than focusing on one problem we focus on wide range of issues associated with the same.</p>
-.</p>
-<div class='link'>
-<a href='blog1.php?id=5'>Read more </a><i class='fas fa-long-arrow-alt-right'></i>
-</div>
-</div>
-</div>
-</div><div class='col-md-4 col-sm-6'>
-<div class='single-blog'>
-<figure>
-<img src='slide/image_01.jpg'>
-</figure>
-<div class='blog-detail'>
-<small>By Admin | Sat 15th May 2021</small>
-<h4>Role Of Education In Society</h4>
-<p> <p>Education is the social institution through which social provides its members with important knowledge such as culture norm values, job skills and basic facts. It plays a fundamental role in the construction of a good society.</p>
-.</p>
-<div class='link'>
-<a href='blog1.php?id=4'>Read more </a><i class='fas fa-long-arrow-alt-right'></i>
-</div>
-</div>
-</div>
-</div><div class='col-md-4 col-sm-6'>
-<div class='single-blog'>
-<figure>
-<img src='slide/image_02.jpg'>
-</figure>
-<div class='blog-detail'>
-<small>By Admin | Thu 13th May 2021</small>
-<h4>Personal Hygiene And Its Quintessential Roles To Health Enhancement</h4>
-<p> <p>One of the effective ways that can protect you from the prevailing diseases and problems is personal hygiene. It basically refers to the set of practices perceived by a community to be associated with the preservation of health and healthy living.</p>
-.</p>
-<div class='link'>
-<a href='blog1.php?id=3'>Read more </a><i class='fas fa-long-arrow-alt-right'></i>
-</div>
-</div>
-</div>
-</div>
-</div>
-</div>
-</section>
-<footer class="footer">
-<div class="container">
-<div class="row">
-<div class="col-md-4 col-sm-12">
-<h2>About Us</h2>
-<p>
-Ngo is a leading provider of information technology, consulting, and business process services. Our dedicated employees offer strategic insights, technological expertise and industry experience.
-</p>
-<p>We focus on technologies that promise to reduce costs, streamline processes and speed time-to-market, Backed by our strong quality processes and rich experience managing global... </p>
-</div>
-<div class="col-md-4 col-sm-12">
-<h2>Useful Links</h2>
-<ul class="list-unstyled link-list">
-<li><a ui-sref="about" href="about_us.php">About us</a><i class="fa fa-angle-right"></i></li>
-<li><a ui-sref="portfolio" href="gallery.php">Gallery</a><i class="fa fa-angle-right"></i></li>
-<li><a ui-sref="products" href="ourwork.php">Our Work</a><i class="fa fa-angle-right"></i></li>
-<li><a ui-sref="gallery" href="donate.php">Donate</a><i class="fa fa-angle-right"></i></li>
-<li><a ui-sref="contact" href="contact_us.php">Contact us</a><i class="fa fa-angle-right"></i></li>
-</ul>
-</div>
-<div class="col-md-4 col-sm-12 map-img">
-<h2>Contact Us</h2>
-<address class="md-margin-bottom-40">
-Address:- Deep ji ki Badi<br>
-Phone: 9079646229<br>
-Email: <a href="/cdn-cgi/l/email-protection#b8d6dfd789f8dfd5d9d1d496dbd7d5" class=""><span class="__cf_email__" data-cfemail="d2bcb5bde392b5bfb3bbbefcb1bdbf">[email&#160;protected]</span></a><br>
-Web: <a href="https://ngo.arunil.in" class="">ngo.arunil.in</a><br>
-<script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script type="text/javascript" src="https://www.freevisitorcounters.com/en/home/counter/824705/t/11"></script>
-</address>
-</div>
-</div>
-</div>
-</footer>
-<div class="copy">
-<div class="container">
-<a href="ngo.arunil.in">2021 &copy; All Rights Reserved | Designed and Developed by Arunil Digital Solution</a>
-<span>
-<a href="https://facebook.com/"> <i class="  fab fa-facebook-square"></i></a>
-<a href="https://instagram.com/"> <i class="fab fa-instagram"></i></a>
-<a href="https://twiter.com/"> <i class="fab fa-twitter"></i></a>
-<a href="https://instagram.com/"> <i class="fab fa-linkedin"></i></a>
-</span>
-</div>
-</div>
-<div style="position:fixed; bottom:26px;right:10px;z-index:9999;">
-<a href="https://api.whatsapp.com/send/?phone=9079646229&text&app_absent=0">
-<img src="wtlogo copy.png" target="_blank " style="width:88px;height:88px;">
-</a>
-</div>
-
-<style>
-      
-      .modal .uploadeimg{
-          
-         width:100px;
-         margin-left:178px;
-      }
-      
-      
-       @media screen and (max-width: 900px){
-.modal .uploadeimg{
-	
-	     
-    margin-left: 100px;
-	
-}
-}
-  </style>
-
-<div class="modal" tabindex="-1" role="dialog" id="myModal">
-<div class="modal-dialog" role="document">
-<div class="modal-content">
-<div class="modal-header">
-<h5 class="modal-title">Add Member</h5>
-<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-<span aria-hidden="true">&times;</span>
-</button>
-</div>
-<div class="modal-body">
-<form method="post" enctype="multipart/form-data">
-<input name="name" class="form-control" required placeholder="Enter Your Name" />
-<input name="number" class="form-control" required placeholder="Enter Your Number" />
-<h5>What Your Work</h5>
-<textarea class="form-control" required name="work"> </textarea>
-<div class="uploadeimg">
-<img src="blank_avatar.jpg
-">
-<input type="file" name="img1" style="padding-top:10px;padding-bottom:10px;margin-left:-50px" required>
-</div>
-<button name="save" type="submit" class="btn btn-success btn-block">Submit</button>
-</form>
-</div>
-</div>
-</div>
-</div>
-</body>
-<script src="assets/js/jquery-3.2.1.min.js"></script>
-<script src="assets/js/popper.min.js"></script>
-<script src="assets/js/bootstrap.min.js"></script>
-<script src="assets/plugins/scroll-fixed/jquery-scrolltofixed-min.js"></script>
-<script src="assets/plugins/slider/js/owl.carousel.min.js"></script>
-<script src="assets/js/script.js"></script>
 </html>
