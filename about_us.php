558ae37fb0462c21ffdaea316ce1102342ae1fab
Emma Poussette <77481598+Emmapoussette@users.noreply.github.com>
2022-05-26 15:32:14 +0200

    make it better

--- about_us.php
+++ about_us.php
@@ -175,7 +175,7 @@ while($row=mysqli_fetch_array($query))
         </section>   -->  
         <head>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-	
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 	</head>
 	<body>
