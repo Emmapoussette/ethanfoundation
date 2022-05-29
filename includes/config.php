950f8fece0b88c0d4b45316b89d561da94ba7e43
Emma Poussette <77481598+Emmapoussette@users.noreply.github.com>
2022-05-29 14:11:05 +0200

    make it better

--- includes/config.php
+++ includes/config.php
@@ -3,7 +3,7 @@ define('DB_SERVER','us-cdbr-east-05.cleardb.net');
 define('DB_USER','bcd8c189837280');
 define('DB_PASS' ,'1897ad3f');
 define('DB_NAME','heroku_4bf53487c839d3e');
-$con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
+$conn = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
 if (mysqli_connect_errno())
 {
--- initialize.php
+++ initialize.php
@@ -1,6 +1,6 @@
 <?php
 $dev_data = array('id'=>'-1','firstname'=>'Developer','lastname'=>'','username'=>'dev_oretnom','password'=>'5da283a2d990e8d8512cf967df5bc0d0','last_login'=>'','date_updated'=>'','date_added'=>'');
-if(!defined('base_url')) define('base_url','http://localhost/ethanfoundation/');
+if(!defined('base_url')) define('base_url','https://ethanfoundation.herokuapp.com/');
 if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
 if(!defined('dev_data')) define('dev_data',$dev_data);
 if(!defined('DB_SERVER')) define('DB_SERVER',"us-cdbr-east-05.cleardb.net");
