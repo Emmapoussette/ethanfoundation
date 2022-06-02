4eb1e0952dc6511846f6daadb6f541bdd516ffdb
Emma Poussette <77481598+Emmapoussette@users.noreply.github.com>
2022-06-02 17:58:57 +0200

    make it better

--- index.php
+++ index.php
@@ -130,16 +130,89 @@ include('admin/includes/config.php');
 
     
      <!-- ################# Events Start Here#######################--->
-    <section class="events">
+     <?php
+include('header.php');
+?>
+  <!--  ************************* Page Title Starts Here ************************** -->
+        
+    <div class="page-nav no-margin row">
         <div class="container">
-            <div class="session-title row">
-            <section class="our-blog">
-         	<div class="container">
-             <section class="our-blog">
-         	<div class="container">
-         		<div class="row session-title">
-        			<h2> Our Events</h2>
+            <div class="row">
+                <h2>Our Events</h2>
+                <ul>
+                    <li> <a href="index.php"><i class="fas fa-home"></i> Home</a></li>
+                    <li><i class="fas fa-angle-double-right"></i> Events</li>
+                </ul>
+            </div>
+        </div>
+    </div>
     
+    
+   
+  <!-- ################# Events Start Here#######################--->
+  <?php
+  if (isset($_GET['pageno'])) {
+    $pageno = $_GET['pageno'];
+} else {
+    $pageno = 1;
+}
+$no_of_records_per_page = 8;
+$offset = ($pageno-1) * $no_of_records_per_page;
+include('admin/add-event.php');
+include('includes/config.php');
+$total_pages_sql = "SELECT COUNT(*) FROM events";
+$result = mysqli_query($conn,$total_pages_sql);
+$total_rows = mysqli_fetch_array($result)[0];
+$total_pages = ceil($total_rows / $no_of_records_per_page);
+/*$result="select events.id as eid,events.EventTitle as eventtitle,events.EventImage,category.CategoryName as category,category.id as
+cid,events.EventDetails as eventdetails,events.PostingDate as postingdate from events left join category on category.id=events.CategoryId
+ where events.Is_Active=1 order by events.id desc ";*/
+/*$query = mysqli_query($conn,$result);
+while ($row = mysqli_fetch_array($query)) {
+    ?>
+   <section class="events">
+    <div class="container">
+        
+    <div class="col-md-12">
+        <div class="event-ro row">
+            <div class="col-md-4 ol-sm-6">
+                <div class="event-box">
+                <img class="card-img-top" src="<?php echo htmlentities($row['EventImage']);?>" 
+                alt="<?php echo htmlentities($row['eventtitle']);?>"> 
+                    <h4><?php echo htmlentities($row['eventdetails']);?></h4>
+                    
+                    <p class="raises"><span>Raised : $34,425</span> / $500,000 </p>
+                    <p class="desic"><?php echo htmlentities($row['eventdetails']);?> </p>
+                    <button class="btn btn-success btn-sm">Donate Now</button>
+                </div>
+          
+        </section> 
+<?php
+}
+?>  */
+                
+                
+//session_start();
+//$conn = mysqli_connect("localhost", "root", "", "ethan");
+
+
+
+?>
+
+<html>
+	<head>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+	</head>
+	<body>
+		<br />
+		<div class="container">
+			<br />
+			<br />
+			<br />
+		
+			<br /><br />
 			<?php
 				$query = "select events.id as eid,events.EventTitle as eventtitle,events.RaisesAmount as raises,events.RestAmount as rest,events.EventImage,category.CategoryName as category,category.id as
         cid,events.EventDetails as eventdetails,events.PostingDate as postingdate from events left join category on category.id=events.CategoryId
@@ -183,7 +256,14 @@ include('admin/includes/config.php');
 					
 		</div>
 	</div>
-         </section>
+	<br /></br>
+      </div>
+
+    </body>
+                 
+  <!--  ************************* Footer Starts Here ************************** -->
+          
+  
 
    
     <!-- ################# Charity Number Starts Here#######################--->
