9be410a170fc5f63c23982e8fbf687fb4b78bbd2
Emma Poussette <77481598+Emmapoussette@users.noreply.github.com>
2022-06-02 15:44:56 +0200

    make it better

--- events.php
+++ events.php
@@ -95,7 +95,7 @@ while ($row = mysqli_fetch_array($query)) {
 			<div class="col-md-4">
 				<form method="post" action="donate.php?action=add&id=<?php echo $row["eid"]; ?>">
 					<div style="border:1px solid #333; background-color:#f1f1f1; border-radius:5px; padding:16px;" align="center">
-          <p class="card-img-top" <?php echo htmlentities($row['eventtitle']);?>>
+          <p class="card-img-top" <?php echo htmlentities($row['EventImage']);?>>
           <img src="<?php echo $row['EventImage'] ?>" class="img-responsive" /><br />
 				
           <img src="<?php echo $row["$imgnewfile"] ?>" class="img-responsive" /><br />
