0270a505898a12b5b50de0f7ecd65ef74290a52b
Emma Poussette <77481598+Emmapoussette@users.noreply.github.com>
2022-06-07 16:54:17 +0200

    make it better

---
+++ admin/add-team.php
@@ -0,0 +1,294 @@
+<?php 
+session_start();
+include('includes/config.php');
+error_reporting(0);
+if(strlen($_SESSION['login'])==0)
+  { 
+header('location:index.php');
+}
+else{
+
+    if (isset($_POST['submit']) && isset($_FILES['my_image'])) {
+        include "config.php";
+    
+        echo "<pre>";
+        print_r($_FILES['my_image']);
+        echo "</pre>";
+    
+        $name=$_POST['name'];
+        $postion=$_POST['position'];
+        $img_name = $_FILES['my_image']['name'];
+        $img_size = $_FILES['my_image']['size'];
+        $tmp_name = $_FILES['my_image']['tmp_name'];
+        $error = $_FILES['my_image']['error'];
+    
+        if ($error === 0) {
+            if ($img_size > 125000) {
+                $em = "Sorry, your file is too large.";
+                header("Location: add-teamphp?error=$em");
+            }else {
+                $img_ex = pathinfo($img_name, PATHINFO_EXTENSION);
+                $img_ex_lc = strtolower($img_ex);
+    
+                $allowed_exs = array("jpg", "jpeg", "png"); 
+    
+                if (in_array($img_ex_lc, $allowed_exs)) {
+                    $new_img_name = uniqid("IMG-", true).'.'.$img_ex_lc;
+                    $img_upload_path = 'uploads/'.$new_img_name;
+                    move_uploaded_file($tmp_name, $img_upload_path);
+    
+                    // Insert into Database
+                    $sql = "INSERT INTO team(name,position,profile) 
+                            VALUES('$name','$position','$new_img_name')";
+                    mysqli_query($conn, $sql);
+                    header("Location: team.php");
+                }else {
+                    $em = "You can't upload files of this type";
+                    header("Location: add-team.php?error=$em");
+                }
+            }
+        }else {
+            $em = "unknown error occurred!";
+            header("Location: add-team.php?error=$em");
+        }
+    
+    }else {
+        header("Location: team.php");
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="A fully featured admin theme which can be used to build CRM, CMS, etc.">
+        <meta name="author" content="Coderthemes">
+
+        <!-- App favicon -->
+        <link rel="shortcut icon" href="assets/images/favicon.ico">
+        <!-- App title -->
+        <title>Ethan-Foundation| Add Acvity</title>
+
+        <!-- Summernote css -->
+        <link href="../plugins/summernote/summernote.css" rel="stylesheet" />
+
+        <!-- Select2 -->
+        <link href="../plugins/select2/css/select2.min.css" rel="stylesheet" type="text/css" />
+
+        <!-- Jquery filer css -->
+        <link href="../plugins/jquery.filer/css/jquery.filer.css" rel="stylesheet" />
+        <link href="../plugins/jquery.filer/css/themes/jquery.filer-dragdropbox-theme.css" rel="stylesheet" />
+
+        <!-- App css -->
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/core.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/components.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/icons.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/pages.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/menu.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/responsive.css" rel="stylesheet" type="text/css" />
+		<link rel="stylesheet" href="../plugins/switchery/switchery.min.css">
+        <script src="assets/js/modernizr.min.js"></script>
+ <script>
+function getSubCat(val) {
+  $.ajax({
+  type: "POST",
+  url: "get_subcategory.php",
+  data:'catid='+val,
+  success: function(data){
+    $("#subcategory").html(data);
+  }
+  });
+  }
+  </script>
+    </head>
+
+
+    <body class="fixed-left">
+
+        <!-- Begin page -->
+        <div id="wrapper">
+
+            <!-- Top Bar Start -->
+           <?php include('includes/topheader.php');?>
+            <!-- ========== Left Sidebar Start ========== -->
+             <?php include('includes/leftsidebar.php');?>
+            <!-- Left Sidebar End -->
+
+
+
+            <!-- ============================================================== -->
+            <!-- Start right Content here -->
+            <!-- ============================================================== -->
+            <div class="content-page">
+                <!-- Start content -->
+                <div class="content">
+                    <div class="container">
+
+
+                        <div class="row">
+							<div class="col-xs-12">
+								<div class="page-title-box">
+                                    <h4 class="page-title">Add Activity </h4>
+                                    <ol class="breadcrumb p-0 m-0">
+                                        <li>
+                                            <a href="#">Post</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Add Activity </a>
+                                        </li>
+                                        <li class="active">
+                                            Add Activity
+                                        </li>
+                                    </ol>
+                                    <div class="clearfix"></div>
+                                </div>
+							</div>
+						</div>
+                        <!-- end row -->
+
+<div class="row">
+<div class="col-sm-6">  
+<!---Success Message--->  
+<?php if($msg){ ?>
+<div class="alert alert-success" role="alert">
+<strong>Well done!</strong> <?php echo htmlentities($msg);?>
+</div>
+<?php } ?>
+
+<!---Error Message--->
+<?php if($error){ ?>
+<div class="alert alert-danger" role="alert">
+<strong>Oh snap!</strong> <?php echo htmlentities($error);?></div>
+<?php } ?>
+
+
+</div>
+</div>
+
+
+
+  <!--  ************************* Page Title Starts Here ************************** -->
+        
+  <div class="page-nav no-margin row">
+        <div class="container">
+            <div class="row">
+                <h2>Our Team</h2>
+                <ul>
+                    <li> <a href="add-team.php"><i class="fas fa-home"></i> Home</a></li>
+                    <li><i class="fas fa-angle-double-right"></i> Add Team</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    
+    
+   
+  <!-- ################# Events Start Here#######################--->
+  <?php
+
+include('includes/config.php');
+?>
+<body>
+	<?php if (isset($_GET['error'])): ?>
+		<p><?php echo $_GET['error']; ?></p>
+	<?php endif ?>
+    <form name="addpost" method="post" enctype="multipart/form-data">
+ <div class="form-group m-b-20">
+<label for="exampleInputEmail1">Full Name</label>
+<input type="text" class="form-control" id="posttitle" name="name" placeholder="Enter title" required>
+</div> <div class="form-group m-b-20">
+<label for="exampleInputEmail1">Position</label>
+<input type="text" class="form-control" id="posttitle" name="position" placeholder="Enter title" required>
+</div>
+    </form>
+
+     <form action="upload.php"
+           method="post"
+           enctype="multipart/form-data">
+
+           <input type="file" 
+                  name="my_image">
+
+           <input type="submit" 
+                  name="submit"
+                  value="Upload">
+     	
+     </form>
+</body>
+</html>
+
+
+           <?php include('includes/footer.php');?>
+
+            </div>
+
+
+            <!-- ============================================================== -->
+            <!-- End Right content here -->
+            <!-- ============================================================== -->
+
+
+        </div>
+        <!-- END wrapper -->
+
+
+
+        <script>
+            var resizefunc = [];
+        </script>
+
+        <!-- jQuery  -->
+        <script src="assets/js/jquery.min.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+        <script src="assets/js/detect.js"></script>
+        <script src="assets/js/fastclick.js"></script>
+        <script src="assets/js/jquery.blockUI.js"></script>
+        <script src="assets/js/waves.js"></script>
+        <script src="assets/js/jquery.slimscroll.js"></script>
+        <script src="assets/js/jquery.scrollTo.min.js"></script>
+        <script src="../plugins/switchery/switchery.min.js"></script>
+
+        <!--Summernote js-->
+        <script src="../plugins/summernote/summernote.min.js"></script>
+        <!-- Select 2 -->
+        <script src="../plugins/select2/js/select2.min.js"></script>
+        <!-- Jquery filer js -->
+        <script src="../plugins/jquery.filer/js/jquery.filer.min.js"></script>
+
+        <!-- page specific js -->
+        <script src="assets/pages/jquery.blog-add.init.js"></script>
+
+        <!-- App js -->
+        <script src="assets/js/jquery.core.js"></script>
+        <script src="assets/js/jquery.app.js"></script>
+
+        <script>
+
+            jQuery(document).ready(function(){
+
+                $('.summernote').summernote({
+                    height: 240,                 // set editor height
+                    minHeight: null,             // set minimum height of editor
+                    maxHeight: null,             // set maximum height of editor
+                    focus: false                 // set focus to editable area after initializing summernote
+                });
+                // Select2
+                $(".select2").select2();
+
+                $(".select2-limiting").select2({
+                    maximumSelectionLength: 2
+                });
+            });
+        </script>
+  <script src="../plugins/switchery/switchery.min.js"></script>
+
+        <!--Summernote js-->
+        <script src="../plugins/summernote/summernote.min.js"></script>
+
+    
+
+
+    </body>
+</html>
